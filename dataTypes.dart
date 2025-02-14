b727187cf6511de54ca6e24ce826b09f8f236431
Davi Murta <davimurta@gmail.com>
2025-02-14 00:01:45 -0300

    Today I learned about variables and data types in Dart

---
+++ dataTypes.dart
@@ -0,0 +1,19 @@
+void main() {
+  //Data Types 
+  // Numbers, Strings, Booleans, Lists, Maps, Dynamic
+
+  //Strings
+  String first = "something"; 
+  print("String $first");
+
+  // Integers
+  int myNum = 41;
+  print("integer: $myNum");
+
+  // doubles
+  double OtherNum = 41.21;
+  print("double: $OtherNum");
+
+  // Booleans
+  bool myBool = true;
+}
---
+++ variables.dart
@@ -0,0 +1,18 @@
+void main() {
+  // Variables
+
+  //String
+  var name = "John";
+  String name2 = "John";
+
+  //Integer
+  var x = 41;
+  int y = 41;
+
+  //dynamic
+  dynamic firstName = "tim";
+
+  //Const and Final
+  const String fullName = "John Elder";
+  final String nickname = "J";
+}
